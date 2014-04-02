3df083b961e20189bec0a53e464fc8aadaac0dce
Simone Busoli <simone.busoli@gmail.com>
2014-04-02 02:54:11 +0200

    More progress on server web interface

--- public/js/server.js
+++ public/js/server.js
@@ -5,14 +5,43 @@ function RepositoryModel(repoUrl) {
   this.repoUrl = repoUrl;
   this.shortName = repoData[3] + '/' + repoData[4];
   this.buildLog = ko.observableArray();
+  this.status = ko.observable();
+  this.displayRunButton = ko.computed(function () {
+    return this.status() !== 'running'
+  }, this);
+  this.displayStopButton = ko.computed(function () {
+    return this.status() === 'running'
+  }, this);
 
-  this.buildStarted = function(data, buildId) {
+  this.runBuild = function() {
+    socket.emit('runBuild', repoUrl);
+  };
+
+  this.stopBuild = function() {
+    socket.emit('stopBuild', repoUrl);
+  };
+
+  this.buildStarted = function (data, buildId) {
+    this.status('running');
     this.buildLog.push('Build started with data ' + JSON.stringify(data));
   };
 
-  this.build = function(message, buildId) {
+  this.build = function (message, buildId) {
     this.buildLog.push(message);
   };
+
+  this.buildError = function (err, buildId) {
+    this.status('error');
+    this.buildLog.push(err.toString());
+  };
+
+  this.buildCompleted = function (buildId) {
+    this.status('completed');
+  };
+
+  this.message = function(message) {
+    this.buildLog.push(message);
+  }
 }
 
 function ServerModel() {
@@ -30,23 +59,33 @@ function ServerModel() {
     self.selectedRepository(self.repositories()[0]);
   };
 
-  self.buildStarted = function(data, buildId) {
+  self.buildStarted = function (data, buildId) {
     var repo = _.find(self.repositories(), { repoUrl: data.repoUrl });
     buildsToRepo[buildId] = repo;
 
     repo.buildStarted(data, buildId);
-  }
+  };
 
-  self.build = function(message, buildId) {
+  self.build = function (message, buildId) {
     buildsToRepo[buildId].build(message, buildId);
   };
+
+  self.buildError = function (err, buildId) {
+    buildsToRepo[buildId].buildError(err, buildId);
+  };
+
+  self.message = function(message, repoUrl) {
+    _.find(self.repositories(), { repoUrl: repoUrl }).message(message);
+  };
 }
+
+var socket;
 
 $(function () {
   var serverModel = new ServerModel();
   ko.applyBindings(serverModel, document.body);
 
-  var socket = io.connect('http:///client');
+  socket = io.connect('http:///client');
 
   socket.on('connect', function () {
     console.log('connected');
@@ -59,7 +98,7 @@ $(function () {
     serverModel.buildStarted(data, buildId);
   });
   socket.on('buildError', function (err, buildId) {
-    console.log('build error')
+    serverModel.buildError(err, buildId);
   });
   socket.on('buildCompleted', function (buildId) {
     console.log('buildCompleted');
@@ -67,10 +106,11 @@ $(function () {
   socket.on('build', function (message, buildId) {
     serverModel.build(message, buildId);
   });
-  socket.on('message', function (message, buildId) {
+  socket.on('message', function (message, repoUrl) {
+    serverModel.message(message, repoUrl);
     console.log(message);
   });
-  socket.on('agentDisconnected', function(agentId, buildId) {
+  socket.on('agentDisconnected', function (agentId, buildId) {
     console.log('agent disconnected');
   });
   socket.on('repos', function (repos) {
