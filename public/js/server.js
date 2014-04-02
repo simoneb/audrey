9e127e61ec66254fccc165ee7a5253f5ded16306
Simone Busoli <simone.busoli@gmail.com>
2014-04-02 20:41:14 +0200

    Better reporting for whole build log

--- public/js/server.js
+++ public/js/server.js
@@ -95,8 +95,8 @@ function BuildModel() {
   };
 
   this.error = function (err) {
-    this.status(Status.error);
-    this.log.push(err.toString());
+    this.ownStatus(Status.error);
+    this.log.push(JSON.stringify(err));
   };
 
   this.message = function (message) {
